4211b759111ccdf8fb6134dea41d42eb6f9091e4
Kip <erikniya@gmail.com>
2025-12-06 06:36:39 +0300

    99% working Profile page

--- Scripts/Data/Scores.js
+++ Scripts/Data/Scores.js
@@ -1,6 +1,7 @@
 class Score {
-    constructor(mapCode, diff, playerID, points, pp, misses) {
+    constructor(mapCode, rank, diff, playerID, points, pp, misses) {
         this.mapCode = mapCode;
+        this.rank = rank;
         this.playerID = playerID;
         this.points = points;
         this.pp = pp;
@@ -9,23 +10,36 @@ class Score {
 }
 
 export const Scores = [
-    new Score("46b75", "Ex+", 76561198427751585, 831774, 517.82, 0),
-    new Score("200dc", "Ex+", 76561198427751585, 1779056, 600.06, 0),
-    new Score("1b4f9", "Ex", 76561198427751585, 82918, 592.55, 0),
-    new Score("20db5", "H", 76561198427751585, 2949819, 537.10, 0),
-    new Score("3d2c5", "N", 76561198427751585, 8735122, 462.23, 0),
-    new Score("13377", "E", 76561198427751585, 572825, 129.24, 0),
-    new Score("1a523", "H", 76561198427751585, 4658473, 235.19, 0),
-    new Score("19059", "Ex+", 76561198427751585, 327985, 612.52, 0),
-    new Score("9e5c", "Ex", 76561198427751585, 439582, 501.25, 0),
-    new Score("1547b", "Ex+", 76561198427751585, 1258342, 622.92, 0),
-    new Score("ffa5", "N", 76561198427751585, 285792, 398.61, 0),
-    new Score("ff83", "Ex", 76561198427751585, 2157823, 125.11, 7),
-    new Score("1d2ef", "Ex+", 76561198427751585, 235672, 482.66, 2),
-    new Score("36c09", "E", 76561198427751585, 98532, 301.25, 0),
-    new Score("3aa01", "Ex", 76561198427751585, 127852, 461.67, 1),
-    new Score("a2de", "Ex+", 76561198427751585, 1287523, 512.43, 4),
-    new Score("168de", "H", 76561198427751585, 3489062, 553.50, 0)
+    new Score("46b75", 41, "Ex+", 76561198427751585, 831774, 517.82, 0),
+    new Score("200dc", 13, "Ex+", 76561198427751585, 1779056, 600.06, 0),
+    new Score("1b4f9", 22, "Ex", 76561198427751585, 82918, 592.55, 0),
+    new Score("20db5", 107, "H", 76561198427751585, 2949819, 537.10, 0),
+    new Score("3d2c5", 15, "N", 76561198427751585, 8735122, 462.23, 0),
+    new Score("13377", 6, "E", 76561198427751585, 572825, 129.24, 0),
+    new Score("1a523", 32, "H", 76561198427751585, 4658473, 235.19, 0),
+    new Score("19059", 51, "Ex+", 76561198427751585, 327985, 612.52, 0),
+    new Score("9e5c", 9, "Ex", 76561198427751585, 439582, 501.25, 0),
+    new Score("1547b", 13, "Ex+", 76561198427751585, 1258342, 622.92, 0),
+    new Score("ffa5", 15, "N", 76561198427751585, 285792, 398.61, 0),
+    new Score("ff83", 22, "Ex", 76561198427751585, 2157823, 125.11, 7),
+    new Score("1d2ef", 111, "Ex+", 76561198427751585, 235672, 482.66, 2),
+    new Score("36c09", 1, "E", 76561198427751585, 98532, 301.25, 0),
+    new Score("3aa01", 5, "Ex", 76561198427751585, 127852, 461.67, 1),
+    new Score("a2de", 7, "Ex+", 76561198427751585, 1287523, 512.43, 4),
+    new Score("168de", 10, "H", 76561198427751585, 3489062, 553.50, 0)
 ]
 
-let fetchedScores = null;
+export let fetchedScores = null;
+
+let ssResponse = null;
+
+await fetch('https://corsproxy.io/?https://scoresaber.com/api/player/76561198427751585/scores?limit=20&sort=top')
+    .then(data => data.json()).then(json => ssResponse = json);
+
+
+fetchedScores = ssResponse.playerScores.map(score => {
+    return {
+        score: score.score,
+        leaderboard: score.leaderboard
+    }
+})
---
+++ Scripts/Profile/ScoresRenderer.js
@@ -0,0 +1,81 @@
+import {fetchedScores} from "../Data/Scores.js"
+import {maps} from "../Data/Maps.js"
+
+function getScoresHTML() {
+    return fetchedScores.map(score => {
+        const currDate = new Date(Date.now());
+        const scoreDate = new Date(score.score.timeSet);
+
+        let finalDiff = '';
+
+        const timeInMs1 = currDate.getTime();
+        const timeInMs2 = scoreDate.getTime();
+        const diffInMs = timeInMs1 - timeInMs2;
+        console.log(currDate);
+
+        const diffInSeconds = Math.trunc(diffInMs / 1000);
+        const diffInMinutes = Math.trunc(diffInMs / (1000 * 60));
+        const diffInHours = Math.trunc(diffInMs / (1000 * 60 * 60));
+        const diffInDays = Math.trunc(diffInMs / (1000 * 60 * 60 * 24));
+        const diffInMonths = Math.trunc(diffInMs / (1000 * 60 * 60 * 24 * 30));
+
+        if (diffInMinutes === 0) {
+            finalDiff = `${diffInSeconds}с`;
+        }
+        else if (diffInHours === 0) {
+            finalDiff = `${diffInMinutes}мин, ${diffInSeconds % 60}с`;
+        }
+        else if (diffInDays === 0) {
+            finalDiff = `${diffInHours}ч, ${diffInMinutes % 60}мин`
+        }
+        else if (diffInMonths === 0) {
+            finalDiff = `${diffInDays}дн, ${diffInSeconds % 24}ч`
+        }
+        else {
+            finalDiff = `${diffInMonths}мес, ${diffInDays % 30}дн`
+        }
+
+        const acc = (score.score.baseScore / score.leaderboard.maxScore * 100).toFixed(2);
+        let pp = score.score.pp == 0 ? '' : `<div class="pp">
+                    <p>${score.score.pp}</p>
+                </div>`;
+
+        return `<div class="score">
+            <div class="scoreinfo1">
+                <div class="rank">
+                    <img src="../Icons/earth.svg" alt="">
+                    <p>#${score.score.rank}</p>
+                </div>
+                <p>${finalDiff}</p>
+            </div>
+            <div class="cover">
+                <img src="${score.leaderboard.coverImage}" alt="">
+            </div>
+            <div class="mapinfo">
+                <a href="">${score.leaderboard.songAuthorName} - ${score.leaderboard.songName}</a>
+                <p>${score.leaderboard.levelAuthorName}</p>
+            </div>
+            <div class="scoreinfo2">
+                <div class="acc">
+                    <p>${acc}%</p>
+                </div>
+                ${pp}
+                <div class="points">
+                    <p>${score.score.baseScore}</p>
+                </div>
+                <div class="mistakes">
+                    <p>FC</p>
+                    <img src="../Icons/checkmark.svg" alt="">
+                </div>
+            </div>
+        </div>`
+    })
+}
+
+function renderScores() {
+    const scoreTable = document.getElementById('scorestable');
+
+    scoreTable.innerHTML = getScoresHTML().join('');
+}
+
+document.addEventListener('DOMContentLoaded', renderScores());
